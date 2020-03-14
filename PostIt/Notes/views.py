84d0fe05d81a8d40e95c91e15872c0674ebc1942
PRITISH009 <pritishgupta009@gmail.com>
2020-03-14 09:12:06 +0530

    Added a Base Template and then extended it to Post-It Wall and About Page.

--- PostIt/Notes/views.py
+++ PostIt/Notes/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(req):
-    return HttpResponse('<h1>Notes Wall</h1>')
+    return render(req, 'Notes/wall.html')
 
 def  about(req):
-    return HttpResponse('<h1>About Page</h1><p>This is a Web App you can use to stick all your notes or tasks to be completed on your own personal wall</p>')
+    return render(req, 'Notes/about.html')
