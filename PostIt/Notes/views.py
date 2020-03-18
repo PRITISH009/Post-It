43d307c0c8bb4ed209359c4aaf4abfa394e1c96c
PRITISH009 <pritishgupta009@gmail.com>
2020-03-19 02:39:06 +0530

    Fixed the Home Page Login Issue

--- PostIt/Notes/views.py
+++ PostIt/Notes/views.py
@@ -9,7 +9,11 @@ context = {
 
 # Create your views here.
 def home(req):
-    return render(req, 'Notes/wall.html', context)
+    if req.user.is_authenticated:
+        return render(req, 'Notes/wall.html', context)
+    else:
+        return render(req, 'Notes/web_app_home.html')
 
 def  about(req):
     return render(req, 'Notes/about.html', {'title' : 'About Page'})
+
